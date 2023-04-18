aef1671efeae784cf7e2380513f455afc79030fd
DanDaeKim <131207269+DanDaeKim@users.noreply.github.com>
2023-04-18 14:50:40 -0700

    Add files via upload

---
+++ da_goat1.0.py
@@ -0,0 +1,138 @@
+import gamelib
+import random
+import math
+import warnings
+from sys import maxsize
+import json
+
+class AlgoStrategy(gamelib.AlgoCore):
+    def __init__(self):
+        super().__init__()
+        seed = random.randrange(maxsize)
+        random.seed(seed)
+        gamelib.debug_write('Random seed: {}'.format(seed))
+
+    def on_game_start(self, config):
+        gamelib.debug_write('Configuring your custom algo strategy...')
+        self.config = config
+        global WALL, SUPPORT, TURRET, SCOUT, DEMOLISHER, INTERCEPTOR, MP, SP
+        WALL = config["unitInformation"][0]["shorthand"]
+        SUPPORT = config["unitInformation"][1]["shorthand"]
+        TURRET = config["unitInformation"][2]["shorthand"]
+        SCOUT = config["unitInformation"][3]["shorthand"]
+        DEMOLISHER = config["unitInformation"][4]["shorthand"]
+        INTERCEPTOR = config["unitInformation"][5]["shorthand"]
+        MP = 1
+        SP = 0
+        self.scored_on_locations = []
+
+    def on_turn(self, turn_state):
+        game_state = gamelib.GameState(self.config, turn_state)
+        gamelib.debug_write('Performing turn {} of your custom algo strategy'.format(game_state.turn_number))
+        game_state.suppress_warnings(True)
+
+        self.build_defenses(game_state)
+        self.build_reactive_defense(game_state)
+        self.deploy_offensive_units(game_state)
+
+        game_state.submit_turn()
+
+    def build_defenses(self, game_state):
+        turret_locations = [[0, 13], [27, 13], [7, 12], [20, 12], [10, 11], [17, 11]]
+        game_state.attempt_spawn(TURRET, turret_locations)
+
+        wall_locations = [[i, 13] for i in range(1, 27) if i != 13 and i != 14]
+        game_state.attempt_spawn(WALL, wall_locations)
+        game_state.attempt_upgrade(wall_locations)
+
+        support_locations = [[i, 12] for i in range(8, 20) if i != 13 and i != 14]
+        game_state.attempt_spawn(SUPPORT, support_locations)
+        game_state.attempt_upgrade(support_locations)
+
+    def build_reactive_defense(self, game_state):
+        for location in self.scored_on_locations:
+            build_location = [location[0], location[1]+1]
+            game_state.attempt_spawn(TURRET, build_location)
+
+    def deploy_offensive_units(self, game_state):
+        if game_state.turn_number < 5:
+            self.stall_with_interceptors(game_state)
+        else:
+            if self.detect_enemy_unit(game_state, unit_type=None, valid_x=None, valid_y=[14, 15]) > 10:
+                self.demolisher_line_strategy(game_state)
+            else:
+                if game_state.turn_number % 2 == 1:
+                    scout_spawn_location_options = [[13, 0], [14, 0]]
+                    best_location = self.least_damage_spawn_location(game_state, scout_spawn_location_options)
+                    game_state.attempt_spawn(SCOUT, best_location, 1000)
+
+                    interceptor_spawn_location_options = [[13, 1], [14, 1]]
+                    best_location = self.least_damage_spawn_location(game_state, interceptor_spawn_location_options)
+                    game_state.attempt_spawn(INTERCEPTOR, best_location, 1000)
+                    support_locations = [[13, 2], [14, 2], [13, 3], [14, 3]]
+                    game_state.attempt_spawn(SUPPORT, support_locations)
+
+    def stall_with_interceptors(self, game_state):
+        friendly_edges = game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_LEFT) + game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_RIGHT)
+        deploy_locations = self.filter_blocked_locations(friendly_edges, game_state)
+        
+        while game_state.get_resource(MP) >= game_state.type_cost(INTERCEPTOR)[MP] and len(deploy_locations) > 0:
+            deploy_index = random.randint(0, len(deploy_locations) - 1)
+            deploy_location = deploy_locations[deploy_index]
+            game_state.attempt_spawn(INTERCEPTOR, deploy_location)
+
+    def demolisher_line_strategy(self, game_state):
+        stationary_units = [WALL, TURRET, SUPPORT]
+        cheapest_unit = WALL
+        for unit in stationary_units:
+            unit_class = gamelib.GameUnit(unit, game_state.config)
+            if unit_class.cost[game_state.MP] < gamelib.GameUnit(cheapest_unit, game_state.config).cost[game_state.MP]:
+                cheapest_unit = unit
+
+        for x in range(27, 5, -1):
+            game_state.attempt_spawn(cheapest_unit, [x, 11])
+
+        game_state.attempt_spawn(DEMOLISHER, [24, 10], 1000)
+
+    def least_damage_spawn_location(self, game_state, location_options):
+        damages = []
+        for location in location_options:
+            path = game_state.find_path_to_edge(location)
+            damage = 0
+            for path_location in path:
+                damage += len(game_state.get_attackers(path_location, 0)) * gamelib.GameUnit(TURRET, game_state.config).damage_i
+            damages.append(damage)
+        
+        return location_options[damages.index(min(damages))]
+
+    def detect_enemy_unit(self, game_state, unit_type=None, valid_x=None, valid_y=None):
+        total_units = 0
+        for location in game_state.game_map:
+            if game_state.contains_stationary_unit(location):
+                for unit in game_state.game_map[location]:
+                    if unit.player_index == 1 and (unit_type is None or unit.unit_type == unit_type) and (valid_x is None or location[0] in valid_x) and (valid_y is None or location[1] in valid_y):
+                        total_units += 1
+        return total_units
+        
+    def filter_blocked_locations(self, locations, game_state):
+        filtered = []
+        for location in locations:
+            if not game_state.contains_stationary_unit(location):
+                filtered.append(location)
+        return filtered
+
+    def on_action_frame(self, turn_string):
+        state = json.loads(turn_string)
+        events = state["events"]
+        breaches = events["breach"]
+        for breach in breaches:
+            location = breach[0]
+            unit_owner_self = True if breach[4] == 1 else False
+            if not unit_owner_self:
+                gamelib.debug_write("Got scored on at: {}".format(location))
+                self.scored_on_locations.append(location)
+                gamelib.debug_write("All locations: {}".format(self.scored_on_locations))
+
+    if __name__ == "__main__":
+        algo = AlgoStrategy()
+        algo.start()
